0d06213519b4e215faf38b81eff9e348ca2afb01
yuzhangbit <yu.zhang.bit@gmail.com>
2018-11-25 05:57:33 +0800

    fix dependencies

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -7,6 +7,8 @@ main() {
   #wget $URL
   export CXX=g++-7
   export CC=gcc-7
+  sudo apt-get update
+  sudo apt-get install build-essential make -y
   git clone --depth 1 --branch ${VERSION} https://github.com/Kitware/CMake.git
   cd CMake && ./bootstrap && make -j4 && sudo make install
   cd /tmp && rm -r CMake
