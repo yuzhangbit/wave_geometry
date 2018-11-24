72c2cc5d7f29bb6c333faa3086ee6d0cdc317df0
yuzhangbit <yu.zhang.bit@gmail.com>
2018-11-25 05:51:35 +0800

    update env

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -5,6 +5,8 @@ VERSION="v3.13.0"
 main() {
   cd /tmp
   #wget $URL
+  export CXX=g++-7
+  export CC=gcc-7
   git clone --depth 1 --branch ${VERSION} https://github.com/Kitware/CMake.git
   cd CMake && ./bootstrap && make -j4 && sudo make install
   cd /tmp && rm -r CMake
